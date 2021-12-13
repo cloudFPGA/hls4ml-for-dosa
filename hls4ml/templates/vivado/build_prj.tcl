1264eafdb5f9c6d24b3396ef77c7682d16a897d0
Burkhard Ringlein <ngl@zurich.ibm.com>
2021-12-13 14:16:42 +0100

    fixing build tcl

--- hls4ml/templates/vivado/build_prj.tcl
+++ hls4ml/templates/vivado/build_prj.tcl
@@ -13,7 +13,7 @@ array set opt {
 
 foreach arg $::argv {
   foreach o [lsort [array names opt]] {
-    regexp "$o=+(\\w+)" $arg unused opt($o)
+    regexp "$o=+(\\w+)" $arg unused $opt($o)
   }
 }
 
